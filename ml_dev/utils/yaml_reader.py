342ed30fda5e600d951e04d649b1c41887fa22d3
Ezzaldin97 <kingtal632@yahoo.com>
2023-08-13 02:24:50 +0300

    add yaml reader module

--- ml_dev/utils/yaml_reader.py
+++ ml_dev/utils/yaml_reader.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     def __init__(self):
-        with open(os.path.join(".", "params.yml"), "r") as f:
+        with open(os.path.join(".", "params.yaml"), "r") as f:
             self.params = yaml.safe_load(f)
-        with open(os.path.join(".", "dvc.yml"), "r") as f:
+        with open(os.path.join(".", "dvc.yaml"), "r") as f:
             self.dvc_pipeline = yaml.safe_load(f)
